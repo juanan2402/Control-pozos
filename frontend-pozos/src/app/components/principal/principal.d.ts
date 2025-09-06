80c5d5447d394d92ed2591074032b7d830ae3317
juanan2402 <juananserrano2402@gmail.com>
2025-09-06 05:22:25 -0500

    diseño de UI

--- frontend-pozos/src/app/components/principal/principal.d.ts
+++ frontend-pozos/src/app/components/principal/principal.d.ts
@@ -1,3 +1,18 @@
-export declare class Principal {
+interface Pozo {
+    nombre: string;
+    ubicacion: string;
+    produccion_diaria: string;
+    estado: 'activo' | 'inactivo';
 }
+export declare class Principal {
+    title: import("@angular/core", { with: { "resolution-mode": "import" } }).WritableSignal<string>;
+    items: import("@angular/core", { with: { "resolution-mode": "import" } }).WritableSignal<Pozo[]>;
+    nombre: import("@angular/core", { with: { "resolution-mode": "import" } }).WritableSignal<string>;
+    ubicacion: import("@angular/core", { with: { "resolution-mode": "import" } }).WritableSignal<string>;
+    produccion_diaria: import("@angular/core", { with: { "resolution-mode": "import" } }).WritableSignal<string>;
+    estado: import("@angular/core", { with: { "resolution-mode": "import" } }).WritableSignal<"" | "activo" | "inactivo">;
+    porcentajeActivos: import("@angular/core", { with: { "resolution-mode": "import" } }).Signal<number>;
+    agregarPozo(): void;
+}
+export {};
 //# sourceMappingURL=principal.d.ts.map
